1768ee05e2b8279608991c037421dfc2ffecabab
alanfreddo <alan.freddo@outlook.com>
2022-02-08 22:05:46 -0300

    Versão beta 1.0 - Melhoria habilitar telnet, ocultar senha digita.

--- telnet_twibi.py
+++ telnet_twibi.py
@@ -2,8 +2,9 @@ from telnetlib import Telnet
 import time
 import requests
 import os
-import sys
 import logging
+import hashlib
+import getpass
 
 ### Menu ####
 
@@ -65,24 +66,44 @@ def largura_2():
 def telnet_habil():
     header('Habilitando telnet, aguarde...')
 
+def telnet_ok():
+    header('Telnet habilitado com sucesso!!!')
+
 #lembrete()
 #time.sleep(3)
 #os.system('cls')
 #header('Projeto Dexter - V1.0')
 
-try:
-    telnet_habil()
-    r = requests.post('http://192.168.5.1/goform/set', json={"login":{"pwd":"25d55ad283aa400af464c76d713c07ad"}})
-    # print(f"Status Code: {r.status_code}, Response: {r.json()}")
-    r = requests.get('http://192.168.5.1/goform/telnet', timeout=10)
-    # print(r.text)
-except Exception:
-    logging.debug('Twibi - telnet enabled!')
-    time.sleep(1)
-
 while True:
+
+    try:
+        senha_admin = getpass.getpass("Digite sua senha de administrador do Twibi: ").encode()
+        os.system('cls')
+        hash = hashlib.md5(senha_admin)
+        encrypt = hash.hexdigest()
+        telnet_habil()
+        # print(encrypt)
+        r = requests.post('http://192.168.5.1/goform/set', json={"login": {"pwd":f'{encrypt}'}})
+        # print(f"Status Code: {r.status_code}, Response: {r.json()}")
+        if f'{r.json()}' == "{'errcode': '1'}":
+            time.sleep(2)
+            os.system('cls')
+            print('\033[31mERRO: Senha incorreta, tente novamente! \033[m')
+            time.sleep(3)
+            os.system('cls')
+            continue
+        else:
+            r = requests.get('http://192.168.5.1/goform/telnet', timeout=10)
+    except Exception:
+        logging.debug('Twibi - telnet enabled!')
+        time.sleep(1)
+    os.system('cls')
+    telnet_ok()
+    time.sleep(2)
+    os.system('cls')
     answer = menu(['Alterar canal do Wi-Fi 5Ghz', 'Alterar canal do Wi-Fi 2.4Ghz', 'Alterar largura de banda 5Ghz', 'Alterar largura de banda 2.4Ghz', 'Desabilitar SIP ALG', 'Sair'])
     if answer == 1:
+        os.system('cls')
         canais_5()
         canal_5 = leiaInt('Digite o canal desejado: ')
         if (canal_5 == 36) or (canal_5 == 40) or (canal_5 == 44) or (canal_5 == 46) or (canal_5 == 48) or (canal_5 == 149) or (canal_5 == 153) or (canal_5 == 157) or (canal_5 == 161):
@@ -104,6 +125,7 @@ while True:
                         OK()
                         time.sleep(3)
                         os.system('cls')
+                        break
                     else:
                         tn.write(b'cfm set wl5g.lock.channel ' + f'{canal_5:"^4}' .encode('ascii') + b'\n')
                         tn.read_until(b"~ # ")
@@ -112,6 +134,7 @@ while True:
                         OK()
                         time.sleep(3)
                         os.system('cls')
+                        break
                 elif pwd_str[3:6] == 'Twi': #TWIBI GIGA Plus
                     tn.write(b'root\r\n')
                     tn.read_until(b"Password:")
@@ -126,6 +149,7 @@ while True:
                         OK()
                         time.sleep(3)
                         os.system('cls')
+                        break
                     else:
                         tn.write(b'cfm set wl5g.lock.channel ' + f'{canal_5:"^4}' .encode('ascii') + b'\n')
                         tn.read_until(b"~ # ")
@@ -134,12 +158,14 @@ while True:
                         OK()
                         time.sleep(3)
                         os.system('cls')
+                        break
         else:
             print('Você digitou um canal inválido, tente novamente.')
             time.sleep(3)
             os.system('cls')
 
     elif answer == 2:
+        os.system('cls')
         canais_2()
         canal_2 = leiaInt('Digite o canal desejado: ')
         if (canal_2 == 1) or (canal_2 == 2) or (canal_2 == 3) or (canal_2 == 4) or (canal_2 == 5) or (canal_2 == 6) or (canal_2 == 7) or(canal_2 == 8) or (canal_2 == 9) or (canal_2 == 10) or (canal_2 == 11) or (canal_2 == 12) or (canal_2 == 13):
@@ -154,24 +180,28 @@ while True:
                     # tn.interact()
                     tn.read_until(b"~ # ")
                     if (canal_2 == 10) or (canal_2 == 11) or (canal_2 == 12) or (canal_2 == 13):
+                        tn.write(b'cfm set auto_channel "0"\n')
+                        tn.read_until(b"~ # ")
                         tn.write(b'cfm set wl2g.lock.channel ' + f'{canal_2:"^4}'.encode('ascii') + b'\n')
                         tn.read_until(b"~ # ")
                         time.sleep(2)
                         tn.write(b'reboot\n')
                         tn.read_until(b"~ # ")
+                        os.system('cls')
                         OK()
                         time.sleep(3)
-                        os.system('cls')
                         break
                     else:
+                        tn.write(b'cfm set auto_channel "0"\n')
+                        tn.read_until(b"~ # ")
                         tn.write(b'cfm set wl2g.lock.channel ' + f'{canal_2:"^3}'.encode('ascii') + b'\n')
                         tn.read_until(b"~ # ")
                         time.sleep(2)
                         tn.write(b'reboot\n')
                         tn.read_until(b"~ # ")
+                        os.system('cls')
                         OK()
                         time.sleep(3)
-                        os.system('cls')
                         break
                 elif pwd_str[3:6] == 'Twi':  # TWIBI GIGA Plus
                     tn.write(b'root\r\n')
@@ -180,30 +210,36 @@ while True:
                     # tn.interact()
                     tn.read_until(b"~ # ")
                     if (canal_2 == 10) or (canal_2 == 11) or (canal_2 == 12) or (canal_2 == 13):
+                        tn.write(b'cfm set auto_channel "0"\n')
+                        tn.read_until(b"~ # ")
                         tn.write(b'cfm set wl2g.lock.channel ' + f'{canal_2:"^4}'.encode('ascii') + b'\n')
                         tn.read_until(b"~ # ")
                         time.sleep(2)
                         tn.write(b'reboot\n')
                         tn.read_until(b"~ # ")
+                        os.system('cls')
                         OK()
                         time.sleep(3)
-                        os.system('cls')
                         break
                     else:
+                        tn.write(b'cfm set auto_channel "0"\n')
+                        tn.read_until(b"~ # ")
                         tn.write(b'cfm set wl2g.lock.channel ' + f'{canal_2:"^3}'.encode('ascii') + b'\n')
                         tn.read_until(b"~ # ")
                         time.sleep(2)
                         tn.write(b'reboot\n')
                         tn.read_until(b"~ # ")
+                        os.system('cls')
                         OK()
                         time.sleep(3)
-                        os.system('cls')
                         break
         else:
-            print('Você digitou um canal inválido, tente novamente.')
+            print('\033[31mERRO: Você digitou um canal inválido, tente novamente! \033[m')
             time.sleep(3)
             os.system('cls')
+            continue
     elif answer == 3:
+        os.system('cls')
         largura_5()
         largura_5 = leiaInt('Qual a largura de banda 5Ghz você deseja: ')
         if (largura_5 == 20) or (largura_5 == 40) or (largura_5 == 80):
@@ -222,9 +258,9 @@ while True:
                     time.sleep(2)
                     tn.write(b'reboot\n')
                     tn.read_until(b"~ # ")
+                    os.system('cls')
                     OK()
                     time.sleep(3)
-                    os.system('cls')
                     break
                 elif pwd_str[3:6] == 'Twi':  # TWIBI GIGA Plus
                     tn.write(b'root\r\n')
@@ -237,15 +273,16 @@ while True:
                     time.sleep(2)
                     tn.write(b'reboot\n')
                     tn.read_until(b"~ # ")
+                    os.system('cls')
                     OK()
                     time.sleep(1)
-                    os.system('cls')
                     break
         else:
             print('Você digitou um canal inválido, tente novamente.')
             time.sleep(3)
             os.system('cls')
     elif answer == 4:
+        os.system('cls')
         largura_2()
         largura_2 = leiaInt('Qual a largura de banda 2.4Ghz você deseja: ')
         if (largura_2 == 20) or (largura_2 == 40):
@@ -264,9 +301,9 @@ while True:
                     time.sleep(2)
                     tn.write(b'reboot\n')
                     tn.read_until(b"~ # ")
+                    os.system('cls')
                     OK()
                     time.sleep(3)
-                    os.system('cls')
                     break
                 elif pwd_str[3:6] == 'Twi':  # TWIBI GIGA Plus
                     tn.write(b'root\r\n')
@@ -279,15 +316,16 @@ while True:
                     time.sleep(2)
                     tn.write(b'reboot\n')
                     tn.read_until(b"~ # ")
+                    os.system('cls')
                     OK()
                     time.sleep(1)
-                    os.system('cls')
                     break
         else:
             print('Você digitou um canal inválido, tente novamente.')
             time.sleep(3)
             os.system('cls')
     elif answer == 5:
+        os.system('cls')
         with Telnet('192.168.5.1', 23, timeout=3) as tn:  # LOGIN TELNET
             # tn.set_debuglevel(1)
             pwd = tn.read_until(b"login:").decode('utf-8')
@@ -300,9 +338,9 @@ while True:
                 tn.read_until(b"~ # ")
                 tn.write(b'echo sip 0 > proc/alg;\n')
                 tn.read_until(b"~ # ")
+                os.system('cls')
                 OK()
                 time.sleep(3)
-                os.system('cls')
             elif pwd_str[3:6] == 'Twi':  # TWIBI GIGA Plus
                 tn.write(b'root\r\n')
                 tn.read_until(b"Password:")
@@ -315,6 +353,7 @@ while True:
                 time.sleep(3)
                 os.system('cls')
     elif answer == 6:
+        os.system('cls')
         header('INTELBRAS, SEMPRE PRÓXIMA.')
         time.sleep(3)
         break
