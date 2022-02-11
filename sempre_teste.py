7cf9f2ab12d1c34a6a384e97b201dda1d905d715
alanfreddo <alan.freddo@outlook.com>
2022-02-11 12:42:42 -0300

    Versão beta 1.0 - Melhoria na lentidão para aplicar as configurações.

--- sempre_teste.py
+++ sempre_teste.py
@@ -1,13 +1,13 @@
-# import getpass4
-import getpass
-
-import requests
-import time
-import logging
+# # import getpass4
 # import getpass
-import getpass
+#
+# import requests
+# import time
+# import logging
+# # import getpass
+# import getpass
+# import socket
+import os
 
-pwd=input()getpass.getpass('Teste: ')
-
-
-
+teste=os.system('ipconfig')
+print(teste)
--- telnet_twibi.py
+++ telnet_twibi.py
@@ -5,6 +5,7 @@ import os
 import logging
 import hashlib
 import getpass
+import netifaces
 
 ### Menu ####
 
@@ -77,6 +78,8 @@ def telnet_ok():
 while True:
 
     try:
+        gateway = netifaces.gateways()
+        default_gateway = gateway['default'][netifaces.AF_INET][0]
         senha_admin = getpass.getpass("Digite sua senha de administrador do Twibi: ").encode()
         os.system('cls')
         hash = hashlib.md5(senha_admin)
