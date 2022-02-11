336c3a66aa8a19e81cc92e81070730888d792b06
alanfreddo <4S06iV0w>
2022-02-11 10:30:52 -0300

    Alteração de teste.

---
+++ testador.py
@@ -0,0 +1,18 @@
+from requests.auth import HTTPBasicAuth
+import requests
+
+### Habilitar telnet #####
+
+print(r.text)
+
+# r = requests.get('http://192.168.6.1/goform/set', auth=HTTPBasicAuth('', '12345678'))
+# print(r.status_code)
+# print(r.text)
+# r = requests.get('http://192.168.6.1/goform/get?module_id=login_pwd')
+# print(r.status_code)
+# print(r.json())
+
+#r = requests.get('http://192.168.6.1/goform/get?module_id=guest_pass')
+#print(r.status_code)
+#r = requests.get('http://192.168.6.1/goform/telnet')
+#print(r.status_code)
